3c451fb25c45c2a8889658656976767e11eed7fa
Kota Prem Sai <23311a04l9@ece.sreenidhi.edu.in>
2024-10-20 17:38:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+let count = 0; 
+let saveEl = document.getElementById("save-el"); 
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+    count++; 
+    document.getElementById("count-el").innerText = count;
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+    let previousEntries = saveEl.innerText;
+    saveEl.innerText = previousEntries + " " + count;
+    count = 0; 
+    document.getElementById("count-el").innerText = count; 
 }
